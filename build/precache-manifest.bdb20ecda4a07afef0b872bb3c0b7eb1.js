5fb6656ade21a2761c542211343de7af5fa968c9
micgro42 <micgro42@users.noreply.github.com>
2019-11-18 00:36:00 +0000

    Release

---
+++ build/precache-manifest.bdb20ecda4a07afef0b872bb3c0b7eb1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd8e741c3a48f1f6d0497410861184f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25ff7b1748a914327bff",
+    "url": "/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "24c44e32ea6da5857c05",
+    "url": "/static/js/2.d4fb5ee1.chunk.js"
+  },
+  {
+    "revision": "25ff7b1748a914327bff",
+    "url": "/static/js/main.f5db92c1.chunk.js"
+  },
+  {
+    "revision": "b6d4595fa70132e6567f",
+    "url": "/static/js/runtime-main.8766d002.js"
+  }
+]);
