93a0d36c7bc35fb6294763515b6f6cbc2bfe6cce
Michael Große <mic.grosse@googlemail.com>
2019-12-22 16:47:08 +0100

    Retrieve tasks from struct lookup
    proof of concept, needs columns in the right order

--- action.php
+++ action.php
@@ -1,10 +1,15 @@
 <?php
+
 /**
  * DokuWiki Plugin personaltodo (Action Component)
  *
  * @license GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
  * @author  Michael Große <mic.grosse6dokuwiki@googlemail.com>
  */
+
+//namespace dokuwiki\plugin\personaltodo;
+
+use dokuwiki\plugin\struct\meta\Search;
 
 class action_plugin_personaltodo extends DokuWiki_Action_Plugin
 {
@@ -46,25 +51,54 @@ class action_plugin_personaltodo extends DokuWiki_Action_Plugin
         $namespace = 'plugin:';
         $pages = [];
         $dirname = dirname(wikiFN($namespace . 'foo'));
-        search($pages, $dirname, 'search_allpages', [ 'depth' => 1]);
-        $ids = array_map(function ($pageResult) use ($namespace) {
-            return $namespace . $pageResult['id'];
-        }, $pages);
+        search($pages, $dirname, 'search_allpages', ['depth' => 1]);
+        $ids = array_map(
+            function ($pageResult) use ($namespace) {
+                return $namespace . $pageResult['id'];
+            }, $pages
+        );
         $projects = [];
         foreach ($ids as $id) {
             $projects[$id] = [
                 'projectId' => $id,
                 'title' => p_get_first_heading(
                     $id
-                )
+                ),
             ];
         }
 
-        $data = json_decode(
-            '{"todos":{"asd":{"todoId":"asd","title":"a hardcoded todo","projectIds":[],"completedDate":null}}}',
-            true
+        $apiColIds = [
+            'todoId',
+            'title',
+            'projectIds',
+            'dueDate',
+            'completedDate',
+        ];
+
+        $structSearch = new Search();
+        $structSearch->addSchema('personaltodo');
+        $structSearch->addColumn('%rowid%');
+        $structSearch->addColumn('*');
+        $tasks = $structSearch->execute();
+        $tasks = array_map(
+            function ($task) use ($apiColIds) {
+                return array_combine(
+                    $apiColIds,
+                    array_map(
+                        function ($value) {
+                            return $value->getRawValue();
+                        },
+                        $task
+                    )
+                );
+            },
+            $tasks
         );
+        $tasks = array_column($tasks, null, 'todoId');
+
+        $data = [];
         $data['projects'] = $projects;
+        $data['todos'] = $tasks;
         //set content type
         header('Content-Type: application/json');
         header('Access-Control-Allow-Origin: *');
