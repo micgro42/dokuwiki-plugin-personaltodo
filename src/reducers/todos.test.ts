e00c43da9562101461d929909bfcfd7f46fd9e75
Michael Große <mic.grosse@googlemail.com>
2019-12-31 22:50:36 +0100

    Better type checking in tests

--- src/reducers/todos.test.ts
+++ src/reducers/todos.test.ts
@@ -1,39 +1,39 @@
 import { TODO_ADD, TODO_COMPLETE, ToDoActions } from '../actions/todoActions';
-import todos from './todos';
+import todos, {Todo} from './todos';
+import {State} from "./rootReducer";
+
+function getExampleState(): State['todos'] {
+    return {
+        foo: {
+            todoId: 'foo',
+            title: 'bar',
+            dueDate: '2019-12-31',
+            projectIds: [],
+            completedDate: null,
+        }
+    };
+}
 
 describe('todos reduce', () => {
     it( 'returns the original state for unknown actions', () => {
-        const incomingState = {
-            foo: {
-                id: 'foo',
-                title: 'bar',
-                projectsIds: [],
-                completedDate: null,
-            }
-        };
+        const incomingState = getExampleState();
+        // @ts-ignore
         const actualState = todos(incomingState, { type: 'unknown' });
         expect(actualState).toBe(incomingState);
     });
 
     it('adds a todo with the corresponding action', () => {
-        const incomingState = {
-            foo: {
-                id: 'foo',
-                title: 'bar',
-                projectsIds: [],
-                completedDate: null,
-            }
-        };
-        const newTodo = {
-            id: 'new',
+        const incomingState = getExampleState();
+        const newTodo: Todo = {
+            todoId: 'new',
             title: 'new Todo',
-            projectsIds: [],
+            projectIds: [],
             completedDate: null,
         };
 
         const expectedState = {
             ...incomingState,
-            [newTodo.id]: newTodo,
+            [newTodo.todoId]: newTodo,
         };
         const testAction: ToDoActions = {
             type: TODO_ADD,
@@ -46,14 +46,7 @@ describe('todos reduce', () => {
     });
 
     it('completes a todo with the corresponding action', () => {
-        const incomingState = {
-            foo: {
-                id: 'foo',
-                title: 'bar',
-                projectsIds: [],
-                completedDate: null,
-            }
-        };
+        const incomingState = getExampleState();
         const testCompletedDate = '2019-11-17';
         const testAction: ToDoActions = {
             type: TODO_COMPLETE,
